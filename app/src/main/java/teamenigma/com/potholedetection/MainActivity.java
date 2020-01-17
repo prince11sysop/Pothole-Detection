e6b3dcb1a12915fbac17b9440433323f1514fffe
prince11sysop <prince11rajput@gmail.com>
2020-01-17 15:30:55 +0530

    Firebase connected

--- app/src/main/java/teamenigma/com/potholedetection/MainActivity.java
+++ app/src/main/java/teamenigma/com/potholedetection/MainActivity.java
@@ -1,5 +1,6 @@
 package teamenigma.com.potholedetection;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -35,8 +36,10 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+
+                startActivity(new Intent(MainActivity.this,MapsActivity.class));
             }
         });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
