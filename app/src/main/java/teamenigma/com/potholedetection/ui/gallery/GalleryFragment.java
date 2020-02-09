993b5f09edc7161b32c5ec25507497a1145f37a3
prince11sysop <prince11rajput@gmail.com>
2020-02-09 16:15:50 +0530

    new commit

--- app/src/main/java/teamenigma/com/potholedetection/ui/gallery/GalleryFragment.java
+++ app/src/main/java/teamenigma/com/potholedetection/ui/gallery/GalleryFragment.java
@@ -6,29 +6,62 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import teamenigma.com.potholedetection.R;
 
-public class GalleryFragment extends Fragment {
+public class GalleryFragment extends Fragment implements OnMapReadyCallback {
 
-    private GalleryViewModel galleryViewModel;
+    private GoogleMap mMap;
+    private double latitude=-34,longitude=151;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        galleryViewModel =
-                ViewModelProviders.of(this).get(GalleryViewModel.class);
         View root = inflater.inflate(R.layout.fragment_gallery, container, false);
-        final TextView textView = root.findViewById(R.id.text_gallery);
-        galleryViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+
         return root;
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(22.7749995,86.1437783);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Jamshedpur"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        LatLng sydney1 = new LatLng(22.7379473, 86.1274197);
+        mMap.addMarker(new MarkerOptions().position(sydney1).title("Marker in Jamshedpur"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney1));
+
+        float zoomLevel = 12.0f; //This goes up to 21
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, zoomLevel));
     }
 }
